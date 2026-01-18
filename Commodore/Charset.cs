20ea9937deca5e0ea7422c8b50acee74371bfae0
roberto ceccarelli <roberto.strawberryfield@gmail.com>
2026-01-18 20:08:55 +0100

    special chars handling

--- Commodore/Charset.cs
+++ Commodore/Charset.cs
@@ -32,7 +32,9 @@ public static class Charset
     /// </summary>
     /// <param name="c">The character to check.</param>
     /// <returns>True if the character is a symbol or number; otherwise, false.</returns>
-    public static bool IsSymbolOrNumber(char c) => (c >= ' ' && c <= '@') || c == 10 || c == 13 || c == 12;
+    public static bool IsSymbolOrNumber(char c) => (c <= '@' && c >= ' ') || c == 10 || c == 13 || c == 12;
+
+    public static bool IsExtendedSymbolOrNumber(char c) => (c <= '@') || (c <= 0x9f && c >= 0x80);
 
     /// <summary>
     /// Determines whether the specified character is an uppercase ASCII letter.
@@ -92,7 +94,7 @@ public static class Charset
     public static string PETSCII(char c, bool LowerCase = false)
     {
         string ret = " ";
-        if (IsSymbolOrNumber(c))
+        if (IsExtendedSymbolOrNumber(c))
         {
             ret = c.ToString();
         }
